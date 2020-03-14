7527255f8167a7394202f4397731c9f7779fa541
jonathan gregis <jon.gregis@gmail.com>
2020-03-14 11:58:28 -0500

    save

--- server.js
+++ server.js
@@ -14,13 +14,11 @@ app.get("/", function(req, res) {
 
 app.use("/api/users", require("./Routes/users"));
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
+// Set static folder
+app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
